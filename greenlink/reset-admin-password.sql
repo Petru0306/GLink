dcd8143b1324f2c38ef4273804ddf6727ec1d135
Petru0306 <petru.panait77@gmail.com>
2025-07-11 15:48:16 +0300

    admin password modify

--- greenlink/reset-admin-password.sql
+++ greenlink/reset-admin-password.sql
@@ -1,4 +1,4 @@
 -- Replace the hash with the one generated from /auth-test/generate-password?password=yourNewPassword
 UPDATE users 
-SET password = '$2a$10$newPasswordHashHere'
+SET password = '$2a$12$r7vlvYOz2bkxGZC4Env95ugHx.NQWMLyMoE8lfU9QsvK8zmPd17Dm'
 WHERE email = 'admin@greenlink.com'; 
